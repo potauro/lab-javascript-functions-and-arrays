afbd87f8ce261e0417d0a5d9b53095fdd9cdb735
massytak <75225037+massytak@users.noreply.github.com>
2021-02-20 14:37:52 +0100

    third iteration

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -38,6 +38,15 @@ console.log (findLongestWord (words))
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
+function sumNumbers(numbers){
+  let sum=0
+  for(i=0;i<numbers.length;i++){
+    sum+=numbers[i]
+    
+  }
+  return sum
+}
+
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
